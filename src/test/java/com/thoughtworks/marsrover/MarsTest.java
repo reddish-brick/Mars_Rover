d079f14c5d2670ac5ff5415592c10932e5f2a0bf
tangmingheng <mhtang@thoughtworks.com>
2020-01-13 16:40:28 +0800

    should_return_direction_S_location_x_10_y_9_when_turn_left_and_move_given_direction_W_location_x_10_y_10

--- src/test/java/com/thoughtworks/marsrover/MarsTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsTest.java
@@ -180,4 +180,15 @@ public class MarsTest {
     assertEquals(10, mars.getLocation().getX().intValue());
     assertEquals(9, mars.getLocation().getY().intValue());
   }
+
+  @Test
+  public void should_return_direction_S_location_x_10_y_9_when_turn_left_and_move_given_direction_W_location_x_10_y_10() {
+    Mars mars = new Mars(new Location(10, 10), Direction.WEST);
+
+    mars.executeCommand(asList(new TurnLeftCommand(), new MoveCommand()));
+
+    assertEquals(Direction.SOUTH, mars.getDirection());
+    assertEquals(10, mars.getLocation().getX().intValue());
+    assertEquals(9, mars.getLocation().getY().intValue());
+  }
 }
