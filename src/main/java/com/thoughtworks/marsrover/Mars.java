3759ade4793c195584b88b47c923a58a5bfdeda1
tangmingheng <mhtang@thoughtworks.com>
2020-01-13 17:07:14 +0800

    refactor: use lombok

--- src/main/java/com/thoughtworks/marsrover/Mars.java
+++ src/main/java/com/thoughtworks/marsrover/Mars.java
@@ -2,28 +2,17 @@ package com.thoughtworks.marsrover;
 
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Mars {
 
   private Location location;
   private DIRECTION direction;
-
-  public Mars(Location location, DIRECTION direction) {
-    this.location = location;
-    this.direction = direction;
-  }
-
-  public Location getLocation() {
-    return location;
-  }
-
-  public DIRECTION getDirection() {
-    return direction;
-  }
-
-  public void setDirection(DIRECTION direction) {
-    this.direction = direction;
-  }
 
   public Mars executeCommand(List<Command> commands) {
     commands.stream()
